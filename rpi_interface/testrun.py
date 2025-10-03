102f4840c5a87fcba1a7e34ef75325c0c00d41a0
JamesRobertJohns <jyng1503@gmail.com>
2025-10-03 18:42:39 +0800

    .

--- rpi_interface/testrun.py
+++ rpi_interface/testrun.py
@@ -539,13 +539,13 @@ class RaspberryPi:
 
         # Handle "NA" or successful recognition
         if results["predicted_id"] == "-1":
-            self.failed_obstacles.append(self.obstacles[results["num_obstacles"]])
+            self.failed_obstacles.append(int(self.obstacles[results["num_obstacles"]]))
             self.logger.info(
                 f"Added Obstacle {results['num_obstacles']} to failed obstacles."
             )
         else:
             obstacle_id = obstacle_id_with_signal.split('_')[0]
-            self.success_obstacles.append(self.obstacles[obstacle_id])
+            self.success_obstacles.append(int(self.obstacles[obstacle_id]))
 
             self.logger.info(
                 f"Obstacle {results['num_obstacles']} successfully recognized."
